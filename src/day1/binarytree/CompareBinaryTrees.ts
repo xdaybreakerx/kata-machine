ca4f2d27dd52dc0206fb73bc19cd8ea2219208a7
Xander Salathe <xsalathe@gmail.com>
2024-09-15 16:49:47 +1000

    feat: compare binary trees

---
+++ src/day1/binarytree/CompareBinaryTrees.ts
@@ -0,0 +1,17 @@
+export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+    
+    // base cases
+    if (a === null && b === null){
+        return true;
+    }
+
+    if (a === null || b === null){
+        return false;
+    }
+
+    if (a.value !== b.value){
+        return false;
+    }
+
+    return compare(a.left, b.left) && compare(a.right, b.right);
+}
